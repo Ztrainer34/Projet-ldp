3cab38a83ff6923d0e47581979475672d8c2478d
Ztrainer34 <151884957+Ztrainer34@users.noreply.github.com>
2024-12-07 17:38:51 +0100

    Update Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -1,59 +1,40 @@
-#include <vector>
-#include "Drawable.cpp"
-#include "Point.h"
-#include "Size.h"
-#include "Ball.cpp"
-using namespace std;
+#include "Block.h"
+#include <allegro5/allegro_primitives.h>
 
-class Block : public virtual Drawable {
-    Point position_block;
-    Size size_block;
-    bool is_visible;
-    const ALLEGRO_COLOR COLOR_GREY = al_map_rgb(200, 200, 200);
+Block::Block(float x, float y, float width, float height, ALLEGRO_COLOR frameColor, ALLEGRO_COLOR fillColor)
+    : position_block{x, y}, size_block{width, height}, frameColor(frameColor), fillColor(fillColor), is_visible(true) {}
 
-public:
-    Block(float x, float y, float width, float height) :
-        position_block{x, y}, size_block{width, height}, is_visible(true) {}
-
-    void draw() override;
-
-    // Handle collision with a ball
-    void handleCollision(Ball& ball) {
-        if (isColliding(ball)) {
-            is_visible = false;
-        }
+void Block::draw() {
+    if (is_visible) {
+        al_draw_filled_rectangle(position_block.x, position_block.y,
+                                 position_block.x + size_block.width,
+                                 position_block.y + size_block.height,
+                                 fillColor);
     }
+}
 
-    // Check if the block and ball are colliding
-    bool isColliding(const Ball& ball) {
-        return ball.getPosition().x + ball.getRadius() > position_block.x && ball.getPosition().x - ball.getRadius()  < position_block.x + size_block.width &&
-               ball.getPosition().y + ball.getRadius() > position_block.y && ball.getPosition().x - ball.getRadius()  < position_block.y + size_block.height;
-    }
+Point Block::getPosition() const {
+    return position_block;
+}
 
-    // Getters and Setters for position_block
-    Point getPosition() const {
-        return position_block;
-    }
+void Block::setPosition(float x, float y) {
+    position_block.x = x;
+    position_block.y = y;
+}
 
-    void setPosition(float x, float y) {
-        position_block.x = x;
-        position_block.y = y;
-    }
+void Block::setPosition(const Point& new_position) {
+    position_block = new_position;
+}
 
-    void setPosition(const Point& new_position) {
-        position_block = new_position;
-    }
+bool Block::getVisibility() const {
+    return is_visible;
+}
 
-    // Additional methods to access visibility and size
-    bool getVisibility() const {
-        return is_visible;
-    }
+void Block::setVisibility(bool visibility) {
+    is_visible = visibility;
+}
 
-    void setVisibility(bool visibility) {
-        is_visible = visibility;
-    }
+Size Block::getSize() const {
+    return size_block;
+}
 
-    Size getSize() const {
-        return size_block;
-    }
-};
