5fbf2d07b6454120772fb41b5b16b51eaa91deff
Zarkax <120319524+Zarkax@users.noreply.github.com>
2024-12-08 19:11:54 +0100

    Create paddle_movement.hpp

---
+++ paddle_movement.hpp
@@ -0,0 +1,9 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+#include <allegro5/allegro.h>
+#include "object.hpp" // ou paddle.hpp
+
+void paddleMovement(ALLEGRO_EVENT event, Paddle &paddle);
+
+#endif // INPUT_HPP
