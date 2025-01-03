a8cfb4c13000a9c275560923c0c08ffbd55fab03
Ztrainer34 <151884957+Ztrainer34@users.noreply.github.com>
2025-01-03 16:13:06 +0100

    Update Capsule.cpp

--- Capsule.cpp
+++ Capsule.cpp
@@ -1,6 +1,7 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include "Capsule.h"
+#include <cmath>
 
 
 
@@ -18,7 +19,13 @@
 
             y_ += 1; // Move capsule downward
         }
-
+    bool Capsule::colors_are_equals(const ALLEGRO_COLOR& c1, const ALLEGRO_COLOR& c2) {
+        constexpr float COLOR_EPSILON = 0.001f;
+        return (std::fabs(c1.r - c2.r) < COLOR_EPSILON &&
+                std::fabs(c1.g - c2.g) < COLOR_EPSILON &&
+                std::fabs(c1.b - c2.b) < COLOR_EPSILON &&
+                std::fabs(c1.a - c2.a) < COLOR_EPSILON);
+    }
 
     bool Capsule::isVisible() const { return visible_; }
     void Capsule::setVisible(bool visible) { visible_ = visible; }
@@ -27,12 +34,29 @@
     float Capsule::getY() const { return y_; }
     float Capsule::getWidth() const { return width_; }
     float Capsule::getHeight() const { return height_; }
-    bool Capsule::checkCollision(Paddle& paddle) const {
-        return x_ < paddle.get_position().x + paddle.get_size().width &&
-               x_ + width_ > paddle.get_position().x &&
-               y_ < paddle.get_position().y + paddle.get_size().height &&
-               y_ + height_ > paddle.get_position().y;
+    ALLEGRO_COLOR Capsule::getColor() const { return color_; }
+    bool Capsule::checkCollision(Paddle& paddle) const{
+        // Capsule's bounding box
+        float capsuleLeft = x_;
+        float capsuleRight = x_ + width_;
+        float capsuleTop = y_;
+        float capsuleBottom = y_ + height_;
+
+        // Paddle's bounding box
+        Point paddlePos = paddle.get_position();
+        Size paddleSize = paddle.get_size();
+        float paddleLeft = paddlePos.x;
+        float paddleRight = paddlePos.x + paddleSize.width;
+        float paddleTop = paddlePos.y;
+        float paddleBottom = paddlePos.y + paddleSize.height;
+
+        // Check if capsule is within the paddle's boundaries
+        if (capsuleBottom >= paddleTop &&     // Capsule's bottom is at or below the paddle's top
+            capsuleTop <= paddleBottom &&     // Capsule's top is at or above the paddle's bottom
+            capsuleRight >= paddleLeft &&     // Capsule's right is at or beyond the paddle's left
+            capsuleLeft <= paddleRight) {     // Capsule's left is at or before the paddle's right
+            return true;                      // Collision detected
+            }
+
+        return false;                         // No collision
     }
-
-
-
